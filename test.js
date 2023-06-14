11430f11b9cc44965fcb489287190d4ef009d745
Camille Chardin <camille.chardin@jimmy-energy.eu>
2023-06-14 18:27:44 +0200

    Essai x

--- test.js
+++
@@ -1,3 +0,0 @@
-var url = "C:/Users/CamilleChardin/workspace/test_mise_en_ligne_site/5ed25af7b1ed23fb00122e13d7f74c4d8262acd8/index.html"
-var request = new XMLHttpRequest()
-request.open('GET', url, true)
